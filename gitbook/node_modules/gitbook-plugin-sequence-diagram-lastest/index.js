0362fc90a0aeb4774168ad078c05f04d64279000
Hongyu <hyinn@live.com>
2019-08-27 00:47:54 +0800

    upddddate

---
+++ gitbook/node_modules/gitbook-plugin-sequence-diagram-lastest/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    book: {
+        assets: "./book",
+        js: [
+            "jquery-2.1.1.min.js",
+            "underscore-min.js",
+            "snap.svg-min.js",
+            "webfont.js",
+            "sequence-diagram-min.js",
+            "plugin.js"
+        ],
+        css: [
+          "styles.css"
+        ],
+        html: {
+            "head:end": function() {
+                // Required to including this way because raphael is non-AMD compliant
+                return "<script src='//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js'></script>";
+            }
+        }
+    }
+};
